2ca28826b0d56c8c8c3b9554fd6f25b3e4663f86
krishnakumar b <79183768+krishnakumarbhat@users.noreply.github.com>
2023-04-24 09:49:48 +0700

    Update 1.led.py

--- 1.led.py
+++ 1.led.py
@@ -1,1 +1,13 @@
+import machine
+import time
+
+# Set up LED pin
+led_pin = machine.Pin('LED', machine.Pin.OUT)
+
+# Blink LED
+while True:
+    led_pin.value(1)  # Turn LED on
+    time.sleep(0.5)   # Wait for 500 milliseconds
+    led_pin.value(0)  # Turn LED off
+    time.sleep(0.5)   # Wait for 500 milliseconds
 
