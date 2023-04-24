353bfb28e5400d14c381c312120266a2a47757be
krishnakumar b <79183768+krishnakumarbhat@users.noreply.github.com>
2023-04-24 09:52:16 +0700

    Update 2.wificonnect.py

--- 2.wificonnect.py
+++ 2.wificonnect.py
@@ -1,1 +1,21 @@
+import network
+
+# Set the SSID and password for your Wi-Fi network
+ssid = 'your_network_ssid'
+password = 'your_network_password'
+
+# Connect to Wi-Fi network
+def connect():
+    global ssid, password
+    station = network.WLAN(network.STA_IF)
+    station.active(True)
+    if not station.isconnected():
+        print('Connecting to network...')
+        station.connect(ssid, password)
+        while not station.isconnected():
+            pass
+    print('Network config:', station.ifconfig())
+
+# Call the connect function to initiate the Wi-Fi connection
+connect()
 
