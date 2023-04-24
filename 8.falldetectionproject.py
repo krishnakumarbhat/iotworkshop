92ba5daad29e3a8abf0f24214e2231b045cbbfe0
krishnakumar b <79183768+krishnakumarbhat@users.noreply.github.com>
2023-04-24 10:06:27 +0700

    Update and rename falldetectionproject.py to 8.falldetectionproject.py

--- 8.falldetectionproject.py
+++ 8.falldetectionproject.py
@@ -1,32 +1,35 @@
+# Import necessary libraries
 from machine import Pin, I2C
 import time
 import math
 import network
 
-ssid = "guru"  # Enter your WiFi Name
-password = "guru1234"  # Enter your WiFi Password
+# Define WiFi network information
+ssid = "krishk"  # Enter your WiFi Name
+password = "kris1234"  # Enter your WiFi Password
 
-# WiFi network info.
+# Function to connect to WiFi
 def connect_wifi():
     global ssid, password
     station = network.WLAN(network.STA_IF)
     station.active(True)
     station.connect(ssid, password)
-    while not station.isconnected():
+    while not station.isconnected(): # Loop until WiFi connection is established
         time.sleep(0.5)
         print(".", end="")
     print("\nWiFi connected")
 
-# IFTTT Maker Webhooks info.
+# Define IFTTT Maker Webhooks information
 event = "fall_detect"
 maker_key = "cMvMC9ExTf9wWFtZqxHEXK1cDEP7eLPWOuqBPfRM0_U"
 maker_url = "https://maker.ifttt.com/trigger/" + event + "/with/key/" + maker_key
 
-# MPU-6050 sensor info.
+# Initialize MPU-6050 sensor information
 i2c = I2C(0,sda=Pin(4), scl=Pin(5))
 MPU_addr = 0x68
 i2c.writeto_mem(MPU_addr, 0x6B, bytearray([0]))
 
+# Initialize variables
 AcX = AcY = AcZ = GyX = GyY = GyZ = 0.0 
 ax = ay = az = gx = gy = gz = 0.0
 Amp=0
@@ -35,7 +38,7 @@ fall = trigger1 = trigger2 = False
 trigger1count = trigger2count = 0
 angleChange = 0
 
-
+# Function to read sensor data
 def mpu_read():
     global ax, ay, az, gx, gy, gz
     global AcX,AcY,AcZ,GyX,GyY,GyZ 
@@ -46,8 +49,8 @@ def mpu_read():
     GyX = data[8] << 8 | data[9]
     GyY = data[10] << 8 | data[11]
     GyZ = data[12] << 8 | data[13]
-    
 
+# Function to process sensor data
 def data_processing():
     global ax, ay, az, gx, gy, gz
     global AcX,AcY,AcZ,GyX,GyY,GyZ
@@ -61,6 +64,7 @@ def data_processing():
     Raw_Amp = math.sqrt(ax ** 2 + ay ** 2 + az ** 2)
     Amp = int(Raw_Amp * 10)
 
+# Function to send IFTTT event notification
 def send_event(event):
     import urequests
     try:
