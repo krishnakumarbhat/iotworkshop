cacea2a2a2c35c4b68a65707c058172ce730a1eb
krishnakumar b <79183768+krishnakumarbhat@users.noreply.github.com>
2023-04-24 10:12:55 +0700

    Update 6.httpclient.py

--- 6.httpclient.py
+++ 6.httpclient.py
@@ -1,1 +1,27 @@
+import socket
 
+# Connect to the web server and request temperature and humidity readings
+addr = ('192.168.231.57', 8000)
+s = socket.socket()
+s.connect(addr)
+request = b'GET / HTTP/1.1\r\nHost: ' + bytes(addr[0], 'utf8') + b'\r\n\r\n'
+s.send(request)
+
+# Receive and decode the server response
+response = s.recv(1024).decode('utf8')
+
+# Extract the temperature and humidity readings from the response
+temp_start = response.find('Temperature: ') + len('Temperature: ')
+temp_end = response.find(' C<br>')
+temp = response[temp_start:temp_end]
+
+hum_start = response.find('Humidity: ') + len('Humidity: ')
+hum_end = response.find(' %</p>')
+hum = response[hum_start:hum_end]
+
+# Print the temperature and humidity readings
+print('Temperature:', temp)
+print('Humidity:', hum)
+
+# Close the socket
+s.close()
