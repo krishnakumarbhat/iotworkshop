92d03ed5d318484d8a4d76d22f970a12c8c6cc2d
krishnakumar b <79183768+krishnakumarbhat@users.noreply.github.com>
2023-04-30 10:43:03 +0700

    Update 9.sensehum_in_senshat.py

--- 9.sensehum_in_senshat.py
+++ 9.sensehum_in_senshat.py
@@ -1,1 +1,18 @@
+from sense_hat import SenseHat
+sense= SenseHat () 
+sense.clear()
+humidity = sense.get_humidity()
+print (humidity)
 
+
+from sense_hat import SenseHat 
+sense =SenseHat () 
+sense.clear()
+ptemp = sense.get_temperature_from_pressure()
+htemp =sense.get_temperature()
+temp = (ptemp+htemp)/2
+
+print (ptemp)
+print (htemp)
+print(temp)
+sense.show_message(str(temp) + str(humidity))
